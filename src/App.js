521e5c6283f6171aafa89e7c9feb5cb86683fe54
kicikhaluk <f.halukkicik@outlook.com>
2021-01-31 23:33:37 +0300

    project template created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.less';
+import { Button } from 'antd';
+function App() {
+  return (
+    <>
+      <Button type="primary">Ant Button</Button>
+    </>
+  );
+}
+
+export default App;
