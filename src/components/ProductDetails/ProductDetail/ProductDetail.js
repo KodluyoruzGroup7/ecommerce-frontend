80c8d35b36c23bba9264494322a43b7933992bab
Sinan Dumandzha <sinan.dumanci@gmail.com>
2021-02-11 15:50:08 +0200

    QuantityChanger component added

--- src/components/ProductDetails/ProductDetail/ProductDetail.js
+++ src/components/ProductDetails/ProductDetail/ProductDetail.js
@@ -1,72 +1,83 @@
-import React from 'react';
-import Ratings from '../../UI/Rating/Rating';
+import React from "react";
+import Ratings from "../../UI/Rating/Rating";
+import styles from "./ProductDetail.module.css";
+import QuantityChanger from "../../UI/QuantityChanger/QuantityChanger";
+
 import {
-  faPaperPlane,
-  faHeart,
-  faEnvelope,
-} from '@fortawesome/free-regular-svg-icons';
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
-import styles from './ProductDetail.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+    faPaperPlane,
+    faHeart,
+    faEnvelope,
+} from "@fortawesome/free-regular-svg-icons";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ProductDetail() {
-  return (
-    <div className={styles.detail}>
-      <h2 className={styles.productTitle}>Martha Knit Top</h2>
-      <div className={styles.productStatus}>
-        <Ratings value={3} numberOfReviews={9} />
-        <span className={styles.productStock}>In Stock</span>
-        <span className={styles.productId}>SKU: KG7-EB-84</span>
-      </div>
-      <span className={styles.price}>$300.00</span>
-      <span className={styles.tax}>Tax included.</span>
-      <div className={styles.quantityMessage}>Hurry, Only 4 Left!</div>
-      <div className={styles.colors}>
-        <h3>
-          COLOR: <span className={styles.selectedColor}>GREEN</span>
-        </h3>
-        <div className={styles.colorOptions}>
-          <div className={[styles.colorBox, styles.active].join(' ')}></div>
-          <div className={styles.colorBox}></div>
-          <div className={styles.colorBox}></div>
-          <div className={styles.colorBox}></div>
-          <div className={styles.colorBox}></div>
+    return (
+        <div className={styles.detail}>
+            <h2 className={styles.productTitle}>Martha Knit Top</h2>
+            <div className={styles.productStatus}>
+                <Ratings value={3} numberOfReviews={9} />
+                <span className={styles.productStock}>In Stock</span>
+                <span className={styles.productId}>SKU: KG7-EB-84</span>
+            </div>
+            <span className={styles.price}>$300.00</span>
+            <span className={styles.tax}>Tax included.</span>
+            <div className={styles.quantityMessage}>Hurry, Only 4 Left!</div>
+            <div className={styles.colors}>
+                <h3>
+                    COLOR: <span className={styles.selectedColor}>GREEN</span>
+                </h3>
+                <div className={styles.colorOptions}>
+                    <div
+                        className={[styles.colorBox, styles.active].join(" ")}
+                    ></div>
+                    <div className={styles.colorBox}></div>
+                    <div className={styles.colorBox}></div>
+                    <div className={styles.colorBox}></div>
+                    <div className={styles.colorBox}></div>
+                </div>
+            </div>
+            <div className={styles.sizeContainer}>
+                <h3>
+                    SIZE: <span className={styles.selectedColor}>S</span>
+                </h3>
+                <div className={styles.colorOptions}>
+                    <div className={[styles.sizeBox, styles.active].join(" ")}>
+                        S
+                    </div>
+                    <div className={styles.sizeBox}>M</div>
+                    <div className={styles.sizeBox}>XL</div>
+                </div>
+            </div>
+            <div className={styles.icons}>
+                <div className={styles.icon}>
+                    <FontAwesomeIcon
+                        icon={faHeart}
+                        className={styles.fontawesome}
+                    />
+                    Add to whislist
+                </div>
+                <div className={styles.icon}>
+                    <FontAwesomeIcon
+                        icon={faPaperPlane}
+                        className={styles.fontawesome}
+                    />
+                    Delivery &amp; Returns
+                </div>
+                <div className={styles.icon}>
+                    <FontAwesomeIcon
+                        icon={faEnvelope}
+                        className={styles.fontawesome}
+                    />
+                    Enquiry
+                </div>
+            </div>
+            <div className={styles.actionContainer}>
+                <QuantityChanger />
+                <button>Add to Card</button>
+            </div>
         </div>
-      </div>
-      <div className={styles.sizeContainer}>
-        <h3>
-          SIZE: <span className={styles.selectedColor}>S</span>
-        </h3>
-        <div className={styles.colorOptions}>
-          <div className={[styles.sizeBox, styles.active].join(' ')}>S</div>
-          <div className={styles.sizeBox}>M</div>
-          <div className={styles.sizeBox}>XL</div>
-        </div>
-      </div>
-      <div className={styles.icons}>
-        <div className={styles.icon}>
-          <FontAwesomeIcon icon={faHeart} className={styles.fontawesome} />
-          Add to whislist
-        </div>
-        <div className={styles.icon}>
-          <FontAwesomeIcon icon={faPaperPlane} className={styles.fontawesome} />
-          Delivery &amp; Returns
-        </div>
-        <div className={styles.icon}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.fontawesome} />
-          Enquiry
-        </div>
-      </div>
-      <div className={styles.actionContainer}>
-        <div className={styles.quantityField}>
-          <FontAwesomeIcon icon={faMinus} className={styles.qtyBtn} />
-          <span className={styles.qty}>1</span>
-          <FontAwesomeIcon icon={faPlus} className={styles.qtyBtn} />
-        </div>
-        <button>Add to Card</button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ProductDetail;
--- src/screens/Cart/Cart.js
+++ src/screens/Cart/Cart.js
@@ -1,73 +1,92 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './cart.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import QuantityChanger from "../../components/UI/QuantityChanger/QuantityChanger";
+import "./cart.css";
 
-import data from './cartData';
-import {
-  CloseCircleFilled,
-  MinusOutlined,
-  PlusOutlined,
-} from '@ant-design/icons';
+import data from "./cartData";
+import { CloseCircleFilled } from "@ant-design/icons";
 
 const Cart = () => {
-  return (
-    <>
-      <h1 className='cart-header'>Your Cart</h1>
-      <div className='table-container'>
-        <table class='table'>
-          <thead>
-            <tr className='table-header'>
-              <th></th>
-              <th colspan='2'>PRODUCT</th>
-              <th>PRICE</th>
-              <th>QUANTITY</th>
-              <th>TOTAL</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((product) => (
-              <tr className='table-item'>
-                <td>
-                  <Link to='/' target='_blank' rel='noopener noreferrer'>
-                    <CloseCircleFilled className='delete-icon' />
-                  </Link>
-                </td>
-                <td>
-                  <Link to='/' target='_blank' rel='noopener noreferrer'>
-                    <img src={product.img} alt={product.name} height='100px' />
-                  </Link>
-                </td>
-                <td className='product-text'>
-                  <Link to='/' target='_blank' rel='noopener noreferrer'>
-                    <p className='product-name'>{product.name}</p>
-                    <p className='product-size'>Size: {product.size}</p>
-                    <p className='product-color'>Color: {product.color}</p>
-                  </Link>
-                </td>
-                <td className='price-text'>{product.price} ₺</td>
-                <td className='quantity-text'>
-                  <div>
-                    <div className='quantity'>
-                      <MinusOutlined className='icon' />
-                      <span>1</span>
-                      <PlusOutlined className='icon' />
-                    </div>
-                  </div>
-                </td>
-                <td className='total-text'>{product.price} ₺</td>
-              </tr>
-            ))}
-            <tr>
-              <td className='total-price-text' colspan='6'>
-                <p>Total Price = </p>
-                <p className='price-text-bold'>319.98 ₺</p>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1 className="cart-header">Your Cart</h1>
+            <div className="table-container">
+                <table class="table">
+                    <thead>
+                        <tr className="table-header">
+                            <th></th>
+                            <th colspan="2">PRODUCT</th>
+                            <th>PRICE</th>
+                            <th>QUANTITY</th>
+                            <th>TOTAL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((product) => (
+                            <tr className="table-item">
+                                <td>
+                                    <Link
+                                        to="/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <CloseCircleFilled className="delete-icon" />
+                                    </Link>
+                                </td>
+                                <td>
+                                    <Link
+                                        to="/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <img
+                                            src={product.img}
+                                            alt={product.name}
+                                            height="100px"
+                                        />
+                                    </Link>
+                                </td>
+                                <td className="product-text">
+                                    <Link
+                                        to="/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <p className="product-name">
+                                            {product.name}
+                                        </p>
+                                        <p className="product-size">
+                                            Size: {product.size}
+                                        </p>
+                                        <p className="product-color">
+                                            Color: {product.color}
+                                        </p>
+                                    </Link>
+                                </td>
+                                <td className="price-text">
+                                    {product.price} ₺
+                                </td>
+                                <td className="quantity-text">
+                                    <div className="quantity-changer">
+                                        <QuantityChanger />
+                                    </div>
+                                </td>
+                                <td className="total-text">
+                                    {product.price} ₺
+                                </td>
+                            </tr>
+                        ))}
+                        <tr>
+                            <td className="total-price-text" colspan="6">
+                                <p>Total Price = </p>
+                                <p className="price-text-bold">319.98 ₺</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 };
 
 export default Cart;
