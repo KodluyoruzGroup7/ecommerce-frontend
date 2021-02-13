44f8be2653a796b6e335115b0bc6e0730077f4ee
kicikhaluk <f.halukkicik@outlook.com>
2021-02-13 14:14:40 +0300

    product stories fixed

--- src/components/ProductDetails/ProductSlider/ProductSlider.stories.js
+++ src/components/ProductDetails/ProductSlider/ProductSlider.stories.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import ProductSlider from './ProductSlider';
+import { products } from '../../../data';
 
 export default {
   component: ProductSlider,
-  title: "Components/ProductDetail/ProductSlider"
+  title: 'Components/ProductDetail/ProductSlider',
 };
 
-const Template = (args) => <ProductSlider  {...args} />
+const Template = (args) => <ProductSlider {...args} />;
 
 export const Default = Template.bind({});
+
+Default.args = {
+  images: products[0].images.showcase,
+};
